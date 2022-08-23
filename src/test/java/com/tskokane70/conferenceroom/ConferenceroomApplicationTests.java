233c45a7cf6a10b67e26707cf34065bbf05df171
Tejas Kokane <tskokane70@gmail.com>
2022-08-23 17:40:53 +0530

    Initial commit

---
+++ src/test/java/com/tskokane70/conferenceroom/ConferenceroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tskokane70.conferenceroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
